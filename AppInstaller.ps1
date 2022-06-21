851011a5736e26a92e8bcc90c2af3d43c8d9c405
Joel Wisdom <91797361+theSparro@users.noreply.github.com>
2022-06-20 23:25:41 -0500

    Fix guided installer and update script

--- AppInstaller.ps1
+++ AppInstaller.ps1
@@ -1,53 +1,48 @@
-#     "CPUID.CPU-Z",
-#     "Ubisoft.Uplay",
-#     "WiresharkFoundation.Wireshark"
-#  ) # | ForEach-Object {winget install -e --id $_}
-
-$Browsers = [Ordered]@{
-    "Google.Chrome"=1;
-    "Mozilla.Firefox"=2;
-    "LibreWolf.LibreWolf"=3
-    "BraveSoftware.BraveBrowser"=4;
-}
-$Communication = [Ordered]@{
-    "Discord.Discord"=1;
-    "WhatsApp.WhatsApp"=2;
-    "Mozilla.Thunderbird"=3;
-    "Microsoft.Skype"=4;
-    "SlackTechnologies.Slack"=5;
-    "Microsoft.Teams"=6;
-    "Zoom.Zoom"=7;
-}
-$Development = [Ordered]@{
-    "Git.Git" = 1;
-    "GitHub.Desktop" = 2;
-    "Microsoft.VisualStudioCode"=3;
-    "JetBrains.PyCharm.Professional"=4;
-    "JetBrains.PyCharm.Community"=5;
-    "OpenJS.Nodejs"=6;
-    "Amazon.AWSCLI"=7;
-    "Microsoft.WindowsTerminal"=8;
-}
-$Media = [Ordered]@{
-    "Spotify.Spotify"=1;
-    "CodecGuide.K-LiteCodecPack.Standard"=2;
-    "qBittorrent.qBittorrent"=3;
-}
-$Gaming = [Ordered]@{
-    "Valve.Steam" = 1;
-    "Epic.EpicGamesLauncher" = 2;
-}
-$Utility = [Ordered]@{
-    "7zip.7zip"=1;
-    "RARLab.WinRAR"=2;
-    "Greenshot.Greenshot"=3;
-    "Microsoft.PowerToys"=4;
-    "CPUID.CPU-Z"=5;
-}
-$Networking = [Ordered]@{
-    "VMware.WorksationPlayer"=1;
-    "WiresharkFoundation.Wireshark"=2;
-}
+$Browsers = @(
+    ("Google.Chrome",1),
+    ("Mozilla.Firefox",2),
+    ("LibreWolf.LibreWolf",3),
+    ("BraveSoftware.BraveBrowser",4)
+)
+$Communication = @(
+    ("Discord.Discord", 1),
+    ("WhatsApp.WhatsApp", 2),
+    ("Mozilla.Thunderbird", 3),
+    ("Microsoft.Skype", 4),
+    ("SlackTechnologies.Slack", 5),
+    ("Microsoft.Teams", 6),
+    ("Zoom.Zoom", 7)
+)
+$Development = @(
+    ("Git.Git", 1),
+    ("GitHub.Desktop", 2),
+    ("Microsoft.VisualStudioCode", 3),
+    ("JetBrains.PyCharm.Professional", 4),
+    ("JetBrains.PyCharm.Community", 5),
+    ("OpenJS.Nodejs", 6),
+    ("Amazon.AWSCLI", 7),
+    ("Microsoft.WindowsTerminal", 8)
+)
+$Media = @(
+    ("Spotify.Spotify", 1),
+    ("CodecGuide.K-LiteCodecPack.Standard", 2),
+    ("qBittorrent.qBittorrent", 3)
+)
+$Gaming = @(
+    ("Valve.Steam", 1),
+    ("Epic.EpicGamesLauncher", 2)
+)
+$Utility = @(
+    ("7zip.7zip", 1),
+    ("RARLab.WinRAR", 2),
+    ("Greenshot.Greenshot", 3),
+    ("Microsoft.PowerToys", 4),
+    ("CPUID.CPU-Z", 5)
+)
+$Networking = @(
+    ("VMware.WorksationPlayer", 1),
+    ("WiresharkFoundation.Wireshark", 2)
+)
 $Apps = $Browsers, $Communication, $Development, $Media, $Gaming, $Networking, $Utility
 $Cat_Names = "Browsers", "Communication", "Development", "Media", "Gaming", "Networking", "Utility"
 
@@ -57,12 +52,25 @@ foreach ($Set in $Apps) {
     "`n"
     $Cat_Names[$i]
     "=================="
-    $Set #Print App and Number pairs to console
-    # $Set.Keys | ForEach-Object {
-    #     # "$($Set[$_]): '$_'"
-    #     # $Selection = Read-Host "Enter App Numbers separated by commas or spaces:"
-    #     # winget install -e -h --id $_
-    # }
+    #Print App and Number pairs to console
+    $Set | ForEach-Object {
+        Write-Host $_[1], ".", $_[0] -BackgroundColor DarkGreen -ForegroundColor White
+    }
+    "`n" 
+    $AppNumbers = Read-Host "Enter App Numbers separated by commas or spaces: " 
+    $AppNumbers -split " " | ForEach-Object {
+        $Final_Selection += ($Set[$_-1][0])
+    }
     $i += 1
 }
-
+Write-Host "Finished Selection...`nThese are your selected apps:" -BackgroundColor DarkRed -ForegroundColor White
+Write-Host  $Final_Selection -Separator "`n" -BackgroundColor DarkBlue -ForegroundColor White
+if (Read-Host "Proceed? Y/n" == "n") {
+    Exit-PSSession
+} 
+else {
+    foreach ($App in $Final_Selection) {
+        Write-Host "Installing" $_ 
+        winget install -e -h --id $_
+    }
+}
--- GetMyApps.ps1
+++ GetMyApps.ps1
@@ -2,17 +2,22 @@ $MyAppsList = (
     "Discord.Discord"
  )
 
+winget list | Out-File -FilePath .\Apps.txt
 
-$InstalledApps = winget list
-$InstalledApps = $InstalledApps -join ', '
+$AppsString = ""
+$AppFile = Get-Content -Path .\Apps.txt 
+$AppFile | ForEach-Object {
+    $AppsString += $_
+}
+$AppsString = $AppsString -split " "
 $MyAppsList | ForEach-Object {
-    if ($_ -in $InstalledApps) {
+    if ($_ -in $AppsString) {
         "$_ is already installed."
         "Attempting to Upgrade"
         winget upgrade -h -e --id $_
     }
-    # else {
-    #     "Installing $_"
-    #     winget install -e -h --id $_
-    # }
+    else {
+        "Installing $_"
+        winget install -e -h --id $_
+    }
 }
